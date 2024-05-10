f713157fc0334d672c3cdaba54c50ef54a10bb4f
luqilin <luqilin@4417.com>
2024-05-11 01:11:43 +0800

    update

--- index.95497.js
+++ index.95497.js
@@ -1,4 +1,4 @@
-System.register(["./application.29ed3.js"], function (_export, _context) {
+System.register(["./application.2683f.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -8,8 +8,8 @@ System.register(["./application.29ed3.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application29ed3Js) {
-      Application = _application29ed3Js.Application;
+    setters: [function (_application2683fJs) {
+      Application = _application2683fJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
