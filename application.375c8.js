eb98942e72c9104551ddd815bcc0f1c9448d8d09
luqilin <luqilin@4417.com>
2024-05-17 22:31:55 +0800

    update

--- application.375c8.js
+++ application.375c8.js
@@ -42,7 +42,7 @@ System.register([], function (_export, _context) {
         function Application() {
           _classCallCheck(this, Application);
 
-          this.settingsPath = 'src/settings.3c047.json';
+          this.settingsPath = 'src/settings.6d604.json';
           this.showFPS = false;
         }
 
